e1809ca93b7d8a622671acd994d8376d9af8995a
pathakshivani <pathakshivi2003@gmail.com>
2026-02-11 21:30:10 +0530

    Landing Screen

---
+++ data/constant.ts
@@ -0,0 +1,30 @@
+export const QUICK_VIDEO_SUGGESTIONS = [
+    {
+        id: 1,
+        title: "React Basics",
+        prompt: "Generate a beginner-friendly React.js video course "
+    },
+    {
+        id: 2,
+        title: "Python for Beginners",
+        prompt: "Generate a python begginner tutorial video course "
+    },
+
+    {
+        id: 3,
+        title: "HTML fundamentals",
+        prompt: "Generate a beginner-friendly HTML video course "
+    }
+    ,
+    {
+        id: 4,
+        title: "Tailwind CSS Mastery",
+        prompt: "Generate a Tailwind video course "
+    },
+    {
+        id: 5,
+        title: "Gen AI Pro",
+        prompt: "Generate a Genearative AI video course "
+    }
+]
+
